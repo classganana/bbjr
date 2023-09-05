7438dec541e3520ce1487c4e15ae0844fc203a40
Class Ganana <classganana@gmail.com>
2023-09-06 01:55:21 +0530

    feat: added floating buttons

--- src/screens/quiz/QuizHomePage.tsx
+++ src/screens/quiz/QuizHomePage.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native'
 import { Colors } from '../../styles/colors';
-import { StrongBackButton } from '../../components/common/SvgComponent/SvgComponent';
+import { ClockIcon, StrongBackButton, TestIcon } from '../../components/common/SvgComponent/SvgComponent';
 import { SearchIcon } from '../../components/common/SvgComponent/SvgComponent';
 import Tabs from '../../components/common/Tabs/Tabs';
 import { Card, CardData } from '../../components/quiz/QuizCard';
 
 export const QuizHomePage = () => {
     const [tab, setTab] = useState('Quizzes');
-    const [data, setData] = useState([
+    const [data, setData] = useState<CardData[]>([
         {
-            id: '0',
+            id: 0,
             title: 'Test Your Knowledge on',
             infoText: 'Info about Card 1',
             imageUrl: 'https://example.com/image1.jpg',
@@ -20,7 +20,7 @@ export const QuizHomePage = () => {
             selected: false
         },
         {
-            id: '1',
+            id: 1,
             title: 'Card 2',
             infoText: 'Info about Card 2',
             imageUrl: 'https://example.com/image2.jpg',
@@ -30,25 +30,31 @@ export const QuizHomePage = () => {
             selected: false
         },
         {
-            id: '2',
+            id: 2,
             title: 'Card Cmapis',
             infoText: 'Info about Card 2',
             imageUrl: 'https://example.com/image2.jpg',
             done: false,
             noOfQuestions: 10,
             timeRequired: 20,
-            selected: true
         },
     ]);
+    const [options, setOptions] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
 
-    // useEffect(() => {}, [data])
 
+    useEffect(() => {
+        setOptions(false);
+    },[tab, searchTerm])
+
+    
+    
     const updateList = (index: number) => {
+        setOptions(true);
         let tempData = data;
-        // high order function , map, filter, reduce, some, find,
         tempData = tempData.map((temp) => {
-            if(temp.id != (index as unknown as string)) {
-                    temp.selected = false;
+            if (temp.id != (index)) {
+                temp.selected = false;
             }
             return temp;
         })
@@ -67,7 +73,9 @@ export const QuizHomePage = () => {
                 </View>
                 <View style={styles.infoContainer}>
                     <SearchIcon height={'20'} width={'20'} fill={'#787878'} />
-                    <TextInput style={styles.searchBox} placeholderTextColor={"#808080"} placeholder='Seach Quizzes'></TextInput>
+                    <TextInput style={styles.searchBox}
+                     onChangeText={(text) => {setSearchTerm(text)}}
+                     placeholderTextColor={"#808080"} placeholder='Seach Quizzes'></TextInput>
                 </View>
             </View>
             <View style={styles.tabs}>
@@ -77,12 +85,22 @@ export const QuizHomePage = () => {
                 <Text style={styles.selectedOption}>{tab}</Text>
                 <FlatList
                     data={data}
-                    keyExtractor={(item) => item.id}
+                    keyExtractor={(item) => item.title}
                     renderItem={({ item }) => (
-                        <Card {...item}  onCardClick={(i) => updateList(i)}/>
+                        <Card {...item} onCardClick={(i) => updateList(i)} />
                     )}
                 />
             </View>
+            {options && <View style={styles.floatingButtonContainer}>
+                <View style={styles.floatingButton}>
+                    <TestIcon height={'20'} width={'20'} fill={'black'} />
+                    <Text style={styles.floatingButtonText}>Practice</Text>
+                </View>
+                <View style={styles.floatingButton}>
+                    <ClockIcon height={'20'} width={'20'} fill={'black'} />
+                    <Text style={styles.floatingButtonText}>Take a Test</Text>
+                </View>
+            </View>}
         </View>
     )
 }
@@ -154,5 +172,40 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: '600',
         color: Colors.black_03
+    },
+    floatingButtonContainer: {
+        position: 'absolute',
+        height: 46,
+        width: "80%",
+        bottom: 24,
+        alignSelf: 'center',
+        borderRadius: 25,
+        borderWidth: 0.5,
+        borderColor: '#B1B1B1',
+        backgroundColor: '#FFF',
+        shadowColor: 'rgba(0, 0, 0, 0.15)',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 8,
+        elevation: 1,
+        flexDirection: 'row',
+        overflow: 'hidden'
+    },
+    floatingButton: {
+        flex: 1,
+        borderWidth: 1 / 4,
+        borderColor: "#C5C5C5",
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 8
+    },
+    floatingButtonText: {
+        fontSize: 16,
+        fontWeight: '500',
+        color: Colors.primary
     }
 });
