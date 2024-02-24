9e64718d71761f7391ac8114f89eb4a41a6b400a
Class Ganana <classganana@gmail.com>
2024-02-25 00:36:27 +0530

    feat: added logo and new splash screen

--- src/navigators/OnboardinNavigator.tsx
+++ src/navigators/OnboardinNavigator.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
-import { View } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { ScreenOne } from '../screens/onboarding/ScreenOne';
-import { ScreenTwo } from '../screens/onboarding/ScreenTwo';
-import { ScreenThird } from '../screens/onboarding/ScreenThird';
 import { Onboard } from '../screens/onboarding/Onboard';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const OnBoardingNavigator = () => {
-
   const Stack = createNativeStackNavigator();
+  const [loading, setLoading] = useState(true); // State to manage loading indicator
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkOnboardingStatus = async () => {
+      try {
+        // Simulate async operation
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        // Check if the user has completed onboarding
+        const onboardingCompleted = await AsyncStorage.getItem('onboardingCompleted');
+        if (onboardingCompleted === 'true') {
+          // If onboarding is completed, navigate to another screen
+          navigation.navigate('DashboardNavigator' as never);
+        }
+      } catch (error) {
+        console.error('Error checking onboarding status:', error);
+      } finally {
+        // After the onboarding status check is complete, set loading to false
+        setLoading(false);
+      }
+    };
+
+    checkOnboardingStatus();
+  }, [navigation]);
+
+  if (loading) {
+    // Render loading indicator while loading
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
   return (
   <View style={{flex: 1, width: "100%"}}>
         <Stack.Navigator>
         <Stack.Screen name="ScreenOne" component={Onboard} options={{ headerShown: false }} /> 
-        {/* <Stack.Screen name="ScreenTwo" component={ScreenTwo} options={{ headerShown: false }} /> 
-        <Stack.Screen name="ScreenThird" component={ScreenThird} options={{ headerShown: false }} />  */}
         </Stack.Navigator>
   </View>
   )
